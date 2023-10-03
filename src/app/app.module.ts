cdff0777803c359b160f084e290f78dfc6658f9d
Anjani Kumar <anjani.ies.26@gmail.com>
2023-10-03 23:29:46 +0530

    new component python

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PythonComponent } from './python/python.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PythonComponent
   ],
   imports: [
     BrowserModule,
