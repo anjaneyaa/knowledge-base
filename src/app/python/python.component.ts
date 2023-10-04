55a50d3f974e259e2a483973a0d314c366bbe486
Anjani Kumar <anjani.ies.26@gmail.com>
2023-10-04 10:48:44 +0530

    created coding template

--- src/app/python/python.component.ts
+++ src/app/python/python.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class PythonComponent {
 
+  conditional_statement = `
+  if condition1:
+  # Code to execute if condition1 is True
+  elif condition2:
+  # Code to execute if condition2 is True
+  else:
+  # Code to execute if none of the conditions are True`;
+
+  control_statement = `
+# Conditional Statements
+x = 10
+if x > 0:
+    print("x is positive")
+elif x == 0:
+    print("x is zero")
+else:
+    print("x is negative")
+
+# For Loop
+numbers = [1, 2, 3, 4, 5]
+sum = 0
+for num in numbers:
+    sum += num
+print("Sum:", sum)
+
+# While Loop
+count = 0
+while count < 5:
+    print("Count:", count)
+    count += 1
+
+# Break and Continue
+for i in range(10):
+    if i == 3:
+        break
+    if i % 2 == 0:
+        continue
+    print(i)
+`;
+
+
+
 }
